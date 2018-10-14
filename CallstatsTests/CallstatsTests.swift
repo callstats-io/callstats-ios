9a7d9f694091c01df3f07fe17b5e8b1d5f8d0070
Amornchai Kanokpullwad <zoonref@gmail.com>
2018-10-14 16:33:48 +0800

    add start stop session unit test

--- CallstatsTests/CallstatsTests.swift
+++ CallstatsTests/CallstatsTests.swift
@@ -12,16 +12,47 @@ import XCTest
 class CallstatsTests: XCTestCase {
     
     private var sender: MockEventSender!
+    private var config: CallstatsConfig!
     var callstats: Callstats!
 
     override func setUp() {
+        config = CallstatsConfig()
+        config.keepAlivePeriod = 1
         sender = MockEventSender()
         Callstats.dependency = TestInjector(sender: sender)
-        callstats = Callstats(appID: "app", localID: "local", deviceID: "device", jwt: "code")
+        callstats = Callstats(
+            appID: "app",
+            localID: "local",
+            deviceID: "device",
+            jwt: "code",
+            username: nil,
+            clientVersion: nil,
+            configuration: config)
     }
 
     func testCreateObjectWillDoAuthentication() {
         XCTAssertTrue(sender.lastSendEvent is TokenRequest)
+    }
+    
+    func testStartSessionSendSessionCreateEvent() {
+        callstats.startSession(confID: "conf1")
+        XCTAssertTrue(sender.lastSendEvent is CreateSessionEvent)
+    }
+    
+    func testStartSessionSendKeepAliveEvent() {
+        callstats.startSession(confID: "conf1")
+        let exp = expectation(description: "send ping")
+        let result = XCTWaiter.wait(for: [exp], timeout: config.keepAlivePeriod + 3)
+        if result == XCTWaiter.Result.timedOut {
+            XCTAssertTrue(self.sender.lastSendEvent is KeepAliveEvent)
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func testStopSessionSendUserLeftEvent() {
+        callstats.stopSession()
+        XCTAssertTrue(sender.lastSendEvent is UserLeftEvent)
     }
 }
 
